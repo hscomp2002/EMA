eaaa61f52322576a96c231a6d3f8c6be97aa5f7b
hscomp2002 <hscomp2002@gmail.com>
2021-07-23 15:58:37 +0430

    caculate info

--- index.js
+++ index.js
@@ -1,32 +1,91 @@
-'use strict'
+"use strict"
 
-const Binance = require('node-binance-api');
+require("dotenv").config();
+const ema = require("keltnerchannel").ema;
+const Binance = require("node-binance-api");
 const binance = new Binance().options({
-  APIKEY: 'ZWTtDTaeoqfbhEnXrkn2S4oixDl6KwBXSWgJioCy8hlbFda1K7fNHqt0SHZ0PJ3z',
-  APISECRET: 'O4Hwxfn3ByDfU1xUfXsp7daMc1MCHfkige2eRdfResTiD9dbs7LajKGRCtlAMLig'
+  APIKEY: "ZWTtDTaeoqfbhEnXrkn2S4oixDl6KwBXSWgJioCy8hlbFda1K7fNHqt0SHZ0PJ3z",
+  APISECRET: "O4Hwxfn3ByDfU1xUfXsp7daMc1MCHfkige2eRdfResTiD9dbs7LajKGRCtlAMLig"
 });
 
-function EMACalc(mArray, mRange) {
-  var k = 2 / (mRange + 1);
-  // first item is just the same as the first item in the input
-  let emaArray = [mArray[0]];
-  // for the rest of the items, they are computed with the previous one
-  for (var i = 1; i < mArray.length; i++) {
-    emaArray.push(mArray[i] * k + emaArray[i - 1] * (1 - k));
-  }
-  return emaArray;
+function getSymbolEmaANDLastClose(symbol) {
+  return new Promise((resolve, reject) => {
+    try {
+      binance.candlesticks(symbol,process.env.TIMEFRAME, (error, ticks, symbol) => {
+        let closePriceArray = [];
+        for (let i in ticks) {
+          if (i != ticks.length - 1) {
+            closePriceArray.push(Number(ticks[i][4]));
+          }
+        }
+        let OUT = {};
+        OUT["ema50"] = ema(closePriceArray.slice(150, 200), 50)[0];
+        OUT["ema100"] = ema(closePriceArray.slice(100, 200), 100)[0];
+        OUT["ema200"] = ema(closePriceArray, 200)[0];
+        OUT["lastClose"] = closePriceArray[199];
+        OUT["lastCandleColor"] = closePriceArray[199] < closePriceArray[198] ? "red" : "green";
+        resolve(OUT);
+      }, { limit: 201, endTime: +new Date() });
+    } catch (error) {
+      reject(error);
+    }
+
+  });
 }
 
-// Intervals: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
-binance.candlesticks("BNBUSDT", "15m", (error, ticks, symbol) => {
-  //console.info("candlesticks()", ticks);
-  let closePriceArray = [];
-  for (let i in ticks) {
-    if (i != ticks.length - 1) {
-    closePriceArray.push(ticks[i][4]);
+function calculateSymbolsInfo(symbolInfo) {
+  if (symbolInfo.ema50 > symbolInfo.ema100 && symbolInfo.ema100 > symbolInfo.ema200) {
+    const d50_100 = (symbolInfo.ema50 - symbolInfo.ema100) / ((symbolInfo.ema50 + symbolInfo.ema100)/2);
+    if((d50_100*100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+      return false;
     }
+    const d100_200 = (symbolInfo.ema100 - symbolInfo.ema200) / ((symbolInfo.ema100 + symbolInfo.ema200)/2);
+    if((d100_200 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+      return false;
+    }
+    symbolInfo.marketStatus = "bullish";
+    return symbolInfo;
   }
-  //console.info(closePriceArray);
-  const EMA10 = EMACalc(closePriceArray, 10);
-  console.log(EMA10);
-}, { limit: 11, endTime: +new Date() });
+
+  if (symbolInfo.ema200 > symbolInfo.ema100 && symbolInfo.ema100 > symbolInfo.ema50) {
+    const d50_100 = (symbolInfo.ema100 - symbolInfo.ema50) / ((symbolInfo.ema50 + symbolInfo.ema100)/2);
+    if((d50_100*100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+      return false;
+    }
+    const d100_200 = (symbolInfo.ema200 - symbolInfo.ema200) / ((symbolInfo.ema100 + symbolInfo.ema200)/2);
+    if((d100_200 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+      return false;
+    }
+    symbolInfo.marketStatus = "bearish";
+    return symbolInfo;
+  }
+
+  return false;
+}
+
+async function getSymbolesInfo(){
+  const CURRENCY_LIST = JSON.parse(process.env.CURRENCY_LIST);
+  let symbolsData = [];
+  for (let symbole in CURRENCY_LIST) {
+    let index = symbole;
+    symbole = symbole.replace("/","");
+    let symbolInfo = await getSymbolEmaANDLastClose(symbole);
+    let checkData = calculateSymbolsInfo(symbolInfo);
+    if(!checkData){
+      continue;
+    }
+    symbolsData.push({
+      symbol: symbole,
+      symbolInfo: checkData,
+      botsData: CURRENCY_LIST[index]
+    });
+  }
+  return symbolsData;
+}
+
+async function main() {
+  const symbolesInfo = await getSymbolesInfo();
+  console.log(symbolesInfo);
+}
+
+main();
