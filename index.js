392d4824e3bd753bc49cd9d2d3e14ccd0e9b69dd
hscomp <hscomp2002@gmail.com>
2021-08-21 00:24:46 +0430

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const binance = new Binance().options({
 function EMACalc(mArray,mRange) {
   var k = 2/(mRange + 1);
   // first item is just the same as the first item in the input
-  emaArray = [mArray[0]];
+  let emaArray = [mArray[0]];
   // for the rest of the items, they are computed with the previous one
   for (var i = 1; i < mArray.length; i++) {
     emaArray.push(mArray[i] * k + emaArray[i - 1] * (1 - k));
