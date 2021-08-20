7447ed82ed3d355df34db90bae8b38f19395e046
hscomp <hscomp2002@gmail.com>
2021-08-20 13:15:35 +0430

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const binance = new Binance().options({
 function getSymbolEmaANDLastClose(symbol) {
   return new Promise((resolve, reject) => {
     try {
-      binance.candlesticks(symbol,process.env.TIMEFRAME, (error, ticks, symbol) => {
+      binance.candlesticks(symbol, process.env.TIMEFRAME, (error, ticks, symbol) => {
         let closePriceArray = [];
         for (let i in ticks) {
           if (i != ticks.length - 1) {
@@ -35,12 +35,12 @@ function getSymbolEmaANDLastClose(symbol) {
 
 function calculateSymbolsInfo(symbolInfo) {
   if (symbolInfo.ema20 > symbolInfo.ema50 && symbolInfo.ema50 > symbolInfo.ema100) {
-    const d20_50 = (symbolInfo.ema20 - symbolInfo.ema50) / ((symbolInfo.ema20 + symbolInfo.ema50)/2);
-    if((d20_50*100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+    const d20_50 = (symbolInfo.ema20 - symbolInfo.ema50) / ((symbolInfo.ema20 + symbolInfo.ema50) / 2);
+    if ((d20_50 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT) {
       return false;
     }
-    const d50_100 = (symbolInfo.ema50 - symbolInfo.ema100) / ((symbolInfo.ema50 + symbolInfo.ema100)/2);
-    if((d50_100 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+    const d50_100 = (symbolInfo.ema50 - symbolInfo.ema100) / ((symbolInfo.ema50 + symbolInfo.ema100) / 2);
+    if ((d50_100 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT) {
       return false;
     }
     symbolInfo.marketStatus = "bullish";
@@ -48,12 +48,12 @@ function calculateSymbolsInfo(symbolInfo) {
   }
 
   if (symbolInfo.ema100 > symbolInfo.ema50 && symbolInfo.ema50 > symbolInfo.ema20) {
-    const d20_50 = (symbolInfo.ema50 - symbolInfo.ema20) / ((symbolInfo.ema20 + symbolInfo.ema50)/2);
-    if((d20_50*100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+    const d20_50 = (symbolInfo.ema50 - symbolInfo.ema20) / ((symbolInfo.ema20 + symbolInfo.ema50) / 2);
+    if ((d20_50 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT) {
       return false;
     }
-    const d50_100 = (symbolInfo.ema100 - symbolInfo.ema50) / ((symbolInfo.ema100 + symbolInfo.ema50)/2);
-    if((d50_100 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT){
+    const d50_100 = (symbolInfo.ema100 - symbolInfo.ema50) / ((symbolInfo.ema100 + symbolInfo.ema50) / 2);
+    if ((d50_100 * 100) < process.env.EMA_MINIMUM_DIF_PERCENT) {
       return false;
     }
     symbolInfo.marketStatus = "bearish";
@@ -63,15 +63,15 @@ function calculateSymbolsInfo(symbolInfo) {
   return false;
 }
 
-async function getSymbolesInfo(){
+async function getSymbolesInfo() {
   const CURRENCY_LIST = JSON.parse(process.env.CURRENCY_LIST);
   let symbolsData = [];
   for (let symbole in CURRENCY_LIST) {
     let index = symbole;
-    symbole = symbole.replace("/","");
+    symbole = symbole.replace("/", "");
     let symbolInfo = await getSymbolEmaANDLastClose(symbole);
     let checkData = calculateSymbolsInfo(symbolInfo);
-    if(!checkData){
+    if (!checkData) {
       continue;
     }
     symbolsData.push({
@@ -83,17 +83,102 @@ async function getSymbolesInfo(){
   return symbolsData;
 }
 
-function getEntryPosissionSymbols(symbolesInfo,priceList){
-  for(let i in symbolesInfo){
-    let price = priceList[symbolesInfo[i].symbol];
-    console.log("symbol:",symbolesInfo[i].symbol,":",price);
+function getEntryPosissionSymbols(symbolesInfo) {
+  let signals = [];
+  for (let i in symbolesInfo) {
+
+    let lastClosePrice = symbolesInfo[i].symbolInfo.lastClose;
+    let ema20 = symbolesInfo[i].symbolInfo.ema20;
+    let ema50 = symbolesInfo[i].symbolInfo.ema50;
+    let ema100 = symbolesInfo[i].symbolInfo.ema100;
+    let lastCandleColor = symbolesInfo[i].symbolInfo.lastCandleColor;
+    let marketStatus = symbolesInfo[i].symbolInfo.marketStatus;
+
+    if (isEqual(ema20, lastClosePrice)) {
+      if (marketStatus == "bullish" && lastCandleColor == "red") {
+        // LONG POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.long,
+          sl: ema50,
+          tp: lastClosePrice + (ema20-ema50)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+      else if (marketStatus == "bearish" && lastCandleColor == "green") {
+        // SHORT POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.short,
+          sl: ema50,
+          tp: lastClosePrice - (ema50-ema20)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+    }
+
+
+    if (isEqual(ema50, lastClosePrice)) {
+      if (marketStatus == "bullish" && lastCandleColor == "red") {
+        // LONG POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.long,
+          sl: ema100,
+          tp: lastClosePrice + (ema50-ema100)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+      else if (marketStatus == "bearish" && lastCandleColor == "green") {
+        // SHORT POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.short,
+          sl: ema100,
+          tp: lastClosePrice - (ema100-ema50)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+    }
+
+    if (isEqual(ema100, lastClosePrice)) {
+      if (marketStatus == "bullish" && lastCandleColor == "red") {
+        // LONG POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.long,
+          sl: lastClosePrice - (ema50-ema100) ,
+          tp: lastClosePrice + (ema50-ema100)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+      else if (marketStatus == "bearish" && lastCandleColor == "green") {
+        // SHORT POSITION
+        let tmpSignal = {
+          symbol: symbolesInfo[i].symbol,
+          threeCommasBotId: symbolesInfo[i].botsData.short,
+          sl: lastClosePrice + (ema100-ema50),
+          tp: lastClosePrice - (ema100-ema50)  * 1.5
+        }
+        signals.push(tmpSignal);
+      }
+    }
   }
+  console.log(symbolesInfo);
+  console.log({signals});
+  return signals;
+}
+
+function isEqual(ema, price) {
+  const upSide = ema + ema * process.env.PRICE_AREA;
+  const downSide = ema - ema * process.env.PRICE_AREA;
+  return (price <= upSide && price >= downSide);
 }
 
 async function main() {
-  const priceList = await binance.prices();
+  //const priceList = await binance.prices();
   const symbolesInfo = await getSymbolesInfo();
-  getEntryPosissionSymbols(symbolesInfo,priceList);
+  getEntryPosissionSymbols(symbolesInfo);
 }
 
 main();
