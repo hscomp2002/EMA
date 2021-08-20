6228344b3a15559e0802c30ec5f8011d0dcb7813
hscomp <hscomp2002@gmail.com>
2021-08-21 00:24:01 +0430

    .

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const binance = new Binance().options({
   APISECRET: process.env.APISECRET
 });
 
+function EMACalc(mArray,mRange) {
+  var k = 2/(mRange + 1);
+  // first item is just the same as the first item in the input
+  emaArray = [mArray[0]];
+  // for the rest of the items, they are computed with the previous one
+  for (var i = 1; i < mArray.length; i++) {
+    emaArray.push(mArray[i] * k + emaArray[i - 1] * (1 - k));
+  }
+  return emaArray;
+}
+
 function getSymbolEmaANDLastClose(symbol) {
   return new Promise((resolve, reject) => {
     try {
@@ -21,6 +32,7 @@ function getSymbolEmaANDLastClose(symbol) {
         let OUT = {};
         console.log("last20",closePriceArray.slice(50, 100));
         OUT["ema20"] = ema(closePriceArray.slice(80, 100),20);
+        OUT["ema220"] = EMACalc(closePriceArray.slice(80, 100),[20]);
         OUT["ema50"] = ema(closePriceArray.slice(50, 100),50);
         OUT["ema100"] = ema(closePriceArray, 100);
         OUT["lastClose"] = closePriceArray[99];
